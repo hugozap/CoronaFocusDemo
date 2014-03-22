8727c5b0a889bd2d53bb0e96469faf62819dce34
Hugo Zapata <hugozap@gmail.com>
2014-03-22 04:40:15 -0500

    initial commit

---
+++ demo.lua
@@ -0,0 +1,79 @@
+
+local storyboard = require("storyboard")
+local scene = storyboard.newScene()
+local normal -- normal image
+local mask
+local lens
+
+function scene:createScene()
+
+	local view = self.view
+	local blurred = display.newImageRect( view, "blurred.png", 1024, 748 )
+	blurred.anchorX = 0
+	blurred.anchorY = 0
+
+	normal = display.newImageRect( view, "normal.png", 1024, 748 )
+
+	normal.x = display.contentCenterX
+	normal.y = display.contentCenterY
+
+	lens = display.newImageRect( view, "lens.png", 390, 390 )
+	lens.x = display.contentCenterX
+	lens.y = display.contentCenterY
+
+	mask = graphics.newMask( "mask.png" )
+	--normal.alpha = 0
+	normal:setMask(mask)
+	print("maskX:" .. normal.maskX)
+	--lens.alpha = 0
+	
+	blurred:addEventListener( "touch", function(ev)
+
+		if ev.phase == "began" then
+			normal.hasFocus = true
+			display.getCurrentStage():setFocus(ev.target)
+			--normal.alpha = 1
+			lens.alpha = 1
+			lens.x = ev.x 
+			lens.y = ev.y 
+
+			normal.maskX = ev.x - display.contentCenterX
+			normal.maskY = ev.y - display.contentCenterY
+			
+
+	
+		elseif normal.hasFocus then
+
+			if ev.phase == "moved" then
+				normal.maskX = ev.x - display.contentCenterX
+				normal.maskY = ev.y - display.contentCenterY
+				print("maskX:" .. normal.maskX)
+
+				lens.x = ev.x
+				lens.y = ev.y
+				print("move")
+			elseif ev.phase == "ended" then
+				normal.hasFocus = false
+				display.getCurrentStage():setFocus(nil)
+
+				--normal.alpha = 0
+				--lens.alpha = 0
+			end
+
+			
+
+		end
+
+	end )
+	print("yeah")
+end
+
+function scene:enterFrame()
+
+end
+
+scene:addEventListener( "createScene", scene )
+Runtime:addEventListener( "enterFrame", scene )
+
+
+return scene
